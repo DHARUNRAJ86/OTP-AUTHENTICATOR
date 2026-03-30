9df8f4e60c1e519f24437e7ea0473797fbf8b1de
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-03-30 12:00:14 +0530

    feat: add authentication middleware for logout route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {register,verifyOTP,login,logout} from '../controllers/userController.js'
+import {isAuthenticated} from '../middleware/auth.js'
 
 const router =express.Router();
 
 router.post('/register',register);
 router.post('/otp-verification',verifyOTP);
 router.post('/login',login);
-router.get('/logout',logout);
+router.get('/logout',isAuthenticated,logout);
 
 export default router;
