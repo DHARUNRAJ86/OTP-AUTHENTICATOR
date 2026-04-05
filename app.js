4794c9162f94bcc5049dde2d7897d877b39c9ca9
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-04-05 20:10:03 +0530

    feat: add removeUnverifiedAccounts function to delete unverified user accounts periodically

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ import cors from 'cors'
 import {connection} from './database/dbConnection.js'
 import {errorMiddleware} from './middleware/error.js'
 import userRouter from './routes/userRouter.js'
-
+import {removeUnverifiedAccounts} from './automation/removeUnverifiedAccounts.js'
 
 export const app = express()
 config({path:'./config.env'});
 
+removeUnverifiedAccounts();
 connection();
 
 app.use(cors({
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/v1/user',userRouter);
+
 
 http://localhost:4000/api/v1/user/register
 
