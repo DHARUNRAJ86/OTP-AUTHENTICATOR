ead88f40e0e26ac4710b5c060ae07e23d4c785e1
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-04-07 19:58:42 +0530

    feat: implement authentication context in main component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { createContext,StrictMode,useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
@@ -10,8 +10,19 @@ export const Context = createContext({
      setUser:() =>{}
 })
 
+const AppWrapper = () =>{
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  return(
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+      <App />
+    </Context.Provider>
+  )
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>
 );
