d14fdf91cb1c95e1bfcc847a2027a99fbf1086ae
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-04-11 19:45:38 +0530

    feat: implement login functionality with form handling and error notifications

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -10,10 +10,30 @@ const Login = () => {
   const navigateTo = useNavigate();
 
   const {register,handleSubmit,formState:{errors}} = useForm();
+  const handleLogin = async (date) =>{
+    await axios.post("http://localhost:4000/api/v1/user/login",data,{
+      withCredentials:true,
+      headers:{
+         "Content-Type":"application/json",
+      }
+  }).then((res)=>{
+    toast.success(res.data.message);
+    setIsAuthenticated(true);
+    setUser(res.data.user);
+    navigateTo("/");
+  }).catch((err)=>{
+    toast.error(err.response.data.message);
+  })
+  }
   return <>
-     <form>
+     <form className='auth-form' onSubmit={handleSubmit((data)=>handleLogin(data))}>
       <h2>Login</h2>
-      <input type="email" placeholder="Email" required />
+      <input type="email" placeholder="Email" required {...register("email")}/>
+      <input type="password" placeholder="Password" required {...register("password")}/>
+      <p className="forgot-password">
+        <Link to={'/password/forgot'}>Forgot your password?</Link>
+      </p>
+      <button type='submit'>Login</button>
      </form>
   </>;
 };
