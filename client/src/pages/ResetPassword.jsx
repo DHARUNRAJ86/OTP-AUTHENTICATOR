0ce3467778ed8f34b7289c38796c97dc3048569e
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-04-23 21:35:13 +0530

    feat: fix reset password functionality and improve form structure

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState,useContext} from "react";
 import "../styles/ResetPassword.css";
 import axios from "axios";
 import { Navigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import {Context} from "../main";
+
 
 const ResetPassword = () => {
   const {isAuthenticated,setIsAuthenticated,setUser} = useContext(Context);
@@ -15,8 +16,8 @@ const ResetPassword = () => {
     e.preventDefault();
     try {
     const res = await axios.put(
-      "http://localhost:4000/api/v1/user/password/reset/${token",
-      (password,confirmPassword),
+      `http://localhost:4000/api/v1/user/password/reset/${token}`,
+      { password, confirmPassword },
       {
         withCredentials: true,
         headers: {
@@ -38,7 +39,16 @@ const ResetPassword = () => {
   }
   return (
     <>
-      <></>
+      <div className="reset-password-page">
+        <div className='reset-password-container'>
+          <h2>Reset Password</h2>
+          <p>Enter your new password below.</p>
+          <form className="reset-password-form" onSubmit={handleResetPassword}></form>
+          <input type="password" placeholder="New Password" value={password} onChange={(e)=> setPassword(e.target.value)} required className="reset-input" />
+           <input type="password" placeholder="Confirm New Password" value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)} required className="reset-input" />
+           <button type="submit" className="reset-btn">ResetPassword</button>
+        </div>
+      </div>
     </>
   );
 };
